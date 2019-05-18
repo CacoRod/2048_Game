51e993b5e6a043356b1770f2d1123233fb51ed1e
franciscoMigliardi24 <francisco.migliardi@comunidad.ub.edu.ar>
2019-05-17 21:40:21 -0300

    board con implementacion de movimientos

--- 2048/src/Game/Board.java
+++ 2048/src/Game/Board.java
@@ -8,11 +8,6 @@ public class Board {
 								{new Field(0),new Field(0),new Field(0),new Field(0)}
 							};
 		
-	
-	
-	
-	
-	
 	public Field[][] getTable() {
 		return table;
 	}
@@ -33,18 +28,19 @@ public class Board {
 	
 
 	public void checkSumLeft() {
-		for (int fila=0; fila<=3; fila++) {
-			for (int columna=0; columna<=2;columna++) {
+		for (int fila=0; fila<=table.length - 1; fila++) {
+			for (int columna=0; columna<=table[fila].length - 2;columna++) {
 				for (int siguiente=columna+1;siguiente<=3;siguiente++) {
-					if (getFieldValue(fila,siguiente) != getFieldValue(fila,columna) && (getFieldValue(fila,siguiente) != 0)) siguiente = 3;
+					if (!getField(fila,siguiente).checkSum(getField(fila,columna)) && getFieldValue(fila,siguiente) != 0) break;
+					if (getField(fila,siguiente).checkSum(getField(fila,columna))) {
 						getField(fila,columna).sum(getField(fila,siguiente));
 						getField(fila,siguiente).setValue(0);
-						siguiente = 4;
+						break;
+					}
 					}
 				}
 			}
 		}
-
 
 	public void moveLeft() {
 		checkSumLeft();
@@ -59,7 +55,89 @@ public class Board {
 		}
 	}
 }
+	public void checkSumRight() {
+		for (int fila=0; fila<=3; fila++) {
+			for (int columna=3; columna>=1;columna--) {
+				for (int siguiente=columna-1;siguiente>=0;siguiente--) {
+					if (getFieldValue(fila,siguiente) != getFieldValue(fila,columna) && getFieldValue(fila,siguiente) != 0) break;
+					if (getFieldValue(fila,siguiente) == getFieldValue(fila,columna)) {
+						getField(fila,columna).sum(getField(fila,siguiente));
+						getField(fila,siguiente).setValue(0);
+						break;
+					}
+					}
+				}
+			}
+		}
 
+
+	public void moveRight() {
+		checkSumRight();
+		for (int fila=0; fila<=3; fila++) {
+			for (int contador=1; contador<=4; contador++ ) {
+				for (int columna=0; columna<3;columna++) {
+					if (getFieldValue(fila,columna+1) == 0) {
+						getField(fila, columna+1).sum(getField(fila, columna));
+						getField(fila, columna).setValue(0);	
+				}
+			}
+		}
+	}
+}
+	public void checkSumDown() {
+		for (int columna=0; columna<=3; columna++) {
+			for (int fila=3; fila>=1;fila--) {
+				for (int siguiente=fila-1;siguiente>=0;siguiente--) {
+					if (getFieldValue(siguiente,columna) != getFieldValue(fila,columna) && getFieldValue(siguiente,columna) != 0) break;
+					if (getFieldValue(siguiente,columna) == getFieldValue(fila,columna)) {
+						getField(fila,columna).sum(getField(siguiente,columna));
+						getField(siguiente,columna).setValue(0);
+						break;
+					}
+					}
+				}
+			}
+		}
+	public void moveDown() {
+		checkSumDown();
+		for (int columna=0; columna<=3; columna++) {
+			for (int contador=1; contador<=4; contador++ ) {
+				for (int fila=0; fila<3;fila++) {
+					if (getFieldValue(fila+1,columna) == 0) {
+						getField(fila+1, columna).sum(getField(fila, columna));
+						getField(fila, columna).setValue(0);	
+				}
+			}
+		}
+	}
+}
+	public void checkSumUp() {
+		for (int columna=0; columna<=3; columna++) {
+			for (int fila=0; fila<=2;fila++) {
+				for (int siguiente=fila+1;siguiente<=3;siguiente++) {
+					if (getFieldValue(siguiente,columna) != getFieldValue(fila,columna) && getFieldValue(siguiente,columna) != 0) break;
+					if (getFieldValue(siguiente,columna) == getFieldValue(fila,columna)) {
+						getField(fila,columna).sum(getField(siguiente,columna));
+						getField(siguiente,columna).setValue(0);
+						break;
+					}
+					}
+				}
+			}
+		}
+	public void moveUp() {
+		checkSumUp();
+		for (int columna=0; columna<=3; columna++) {
+			for (int contador=1; contador<=4; contador++ ) {
+				for (int fila=3; fila>0;fila--) {
+					if (getFieldValue(fila-1,columna) == 0) {
+						getField(fila-1, columna).sum(getField(fila, columna));
+						getField(fila, columna).setValue(0);	
+				}
+			}
+		}
+	}
+}
 	public void consoleRender() {
 		for (int fila = 0; fila<=3; fila++) {
 			String rend = "";
--- 2048/src/Game/Main.java
+++ 2048/src/Game/Main.java
@@ -5,19 +5,27 @@ public class Main {
 	public static void main(String[] args) {
 
 		Board test = new Board();
-		test.getField(0, 1).setValue(8);
-		test.getField(0, 2).setValue(8);
-		test.getField(0, 3).setValue(8);
-		test.getField(1, 1).setValue(8);
-		test.getField(1, 3).setValue(8);
-		test.getField(1, 0).setValue(2);
-		test.getField(2, 1).setValue(2);
+		test.getField(0, 0).setValue(2);
+		test.getField(0, 1).setValue(2);
+		test.getField(0, 2).setValue(0);
+		test.getField(0, 3).setValue(2);
+		test.getField(1, 0).setValue(4);
+		test.getField(1, 1).setValue(4);
+		test.getField(1, 2).setValue(4);
+		test.getField(1, 3).setValue(0);
+		test.getField(2, 0).setValue(4);
+		test.getField(2, 1).setValue(4);
+		test.getField(2, 2).setValue(2);
 		test.getField(2, 3).setValue(2);
 		test.consoleRender();
-		test.moveLeft();
+		test.moveUp();
 		System.out.println("\n");
 		test.consoleRender();
-
+		test.moveRight();
+		System.out.println("\n");
+		test.consoleRender();
+		
+	
 	}
 
 }
