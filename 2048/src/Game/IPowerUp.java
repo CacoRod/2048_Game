a1111548fd2a66345bec392e44464549d1ce9e06
Joaquin <mozwho@gmail.com>
2019-05-30 00:08:40 -0300

    arreglo a metodos mal implementados y limpiado de codigo redundante

--- 2048/src/Game/IPowerUp.java
+++ 2048/src/Game/IPowerUp.java
@@ -1,10 +1,6 @@
 package Game;
 
-<<<<<<< HEAD:2048/src/Game/IPowerUp.java
 public interface IPowerUp {
   // los powerups que van a aimplementarse en la partida
-=======
-public class PowerUp {
-	
->>>>>>> master:2048/src/Game/PowerUp.java
+
 }
